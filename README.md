39873b335ab9c0c6356a4a744b0c5c5ea78c881b
Patrick Aldis <patricktaldis@gmail.com>
2025-09-08 15:51:53 +0100

    fix linting

---
+++ README.md
@@ -0,0 +1,4 @@
+# Starting a Nix Shell
+```
+nix-shell --option binary-caches 'https://cache.nixos.org'
+```
--- test/Test/Tests.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Test.DocTest
-
-main :: IO ()
-main = do
-    putStrLn("Running the tests at least")
-    doctest ["LangFeature/KickTyres.hs"]
