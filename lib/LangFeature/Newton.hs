a6cfa7135875cd4f7fbed1e3243b65cad86dfb62
fdilke <fdilke@gmail.com>
2025-09-11 10:55:53 +0100

    refactoring Newton

--- lib/LangFeature/Newton.hs
+++ lib/LangFeature/Newton.hs
@@ -2,8 +2,8 @@ module LangFeature.Newton(mySqrt) where
 
 import Data.List(find)
 
-newt :: Fractional a => a -> a -> a
-newt q x = (x + q/x)/2
+-- newt :: Fractional a => a -> a -> a
+-- newt q x = (x + q/x)/2
 
 closeEnough :: Ord a => Fractional a => a -> a -> a -> Bool
 closeEnough q tolerance est = abs (est * est - q) < tolerance
@@ -12,7 +12,15 @@ standardTolerance :: Fractional a => a
 standardTolerance = 1e-6
 
 mySqrt :: Ord a => Fractional a => a -> a
-mySqrt q = case find (closeEnough q standardTolerance) (iterate (newt q) (q/2)) of
-    Just root -> root
-    Nothing -> -1
+mySqrt q =
+   case find (closeEnough q standardTolerance) (iterate (newt q) (q/2)) of
+     Just root -> root
+     Nothing -> -1
+    where 
+      newt :: Fractional a => a -> a -> a
+      newt p x = (x + p/x)/2
+      
+
+
+
 
