ca81bf71601b1997fa7dabd367915aee4dac7eec
Felix Dilke <felix.dilke@obsidian.systems>
2025-09-22 18:27:37 +0100

    create a basic graph olog, no proper tests

--- test/Test/OlogsSpec.hs
+++ test/Test/OlogsSpec.hs
@@ -17,3 +17,18 @@ spec = do
   describe "function successfully does very little" $ do
     it "mimic (+1) function" $
       doNothing 4 `shouldBe` 4
+
+  describe "create a basic olog" $ do
+    it "the graph olog" $
+      let 
+        -- x :: Int
+        -- x = 3
+        graphOlog :: Olog Int
+        graphOlog = Olog {
+            dots = [0, 1],
+            arcs = [Arc ("source", 0, 1), Arc ("target", 1, 0)],
+            identities = []
+          }
+      in do
+        [7, 8] `shouldBe` [7, 8]
+        
