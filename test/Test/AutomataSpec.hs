1205e6bd6dfac2de0a4bc06ca33217dce2986e3e
Felix Dilke <felix.dilke@obsidian.systems>
2025-09-22 16:54:59 +0100

    functioning automata calculation
    Co-authored-by: George Thomas <georgefsthomas@gmail.com>

--- test/Test/AutomataSpec.hs
+++ test/Test/AutomataSpec.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE NoFieldSelectors #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use const" #-}
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
+{-# HLINT ignore "Avoid lambda" #-}
 module Test.AutomataSpec where
 
 import LangFeature.Automata
@@ -10,11 +15,17 @@ spec = do
   describe "function does nothing" $ do
     it "mimic (+1) function" $
       doNothing 4 `shouldBe` 4
-  describe "george's test" $ do
+  describe "george's test 1" $ do
     it "does stuff" $
-      take 2 (listStates automaton) `shouldBe` [A,B]
+      take 2 (listStates1 automaton1) `shouldBe` [A,B]
+  describe "george's test 2" $ do
+    it "does stuff" $ do
+      take 5 (listStates2 automaton2 ([4,7,12] ++ repeat 1) ) `shouldBe` [0,4,11,23,24]
+      listStates2 automaton2 [4,7,12] `shouldBe`  [0,4,11,23]
 
 data S = A | B | C deriving (Eq, Show)
-automaton :: Automaton S
-automaton = Automaton{initial = A, update = \_ -> B}
--- automaton =  (A,  \_ -> B)
+automaton1 :: Automaton1 S
+automaton1 = Automaton1{initial = A, update = \_ -> B}
+
+automaton2 :: Automaton2 Int Int
+automaton2 = Automaton2{initial = 0, update = \s n -> s + n}
