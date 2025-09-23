c0b3ed9aee8505d1ad7ad33e2efb1b9a170d9899
fdilke <fdilke@gmail.com>
2025-09-23 17:29:00 +0100

    (broken) refactoring the validation

--- lib/LangFeature/Ologs.hs
+++ lib/LangFeature/Ologs.hs
@@ -1,14 +1,16 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE NoFieldSelectors #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# LANGUAGE BlockArguments #-}
+
 {-# HLINT ignore "Eta reduce" #-}
 {-# HLINT ignore "Avoid lambda" #-}
 {-# HLINT ignore "Redundant lambda" #-}
 {-# HLINT ignore "Collapse lambdas" #-}
 {-# HLINT ignore "Unused LANGUAGE pragma" #-}
+{-# LANGUAGE TypeApplications #-}
 
 module LangFeature.Ologs
   ( Arc,
@@ -24,43 +26,36 @@ data Arc dot = Arc
   { name :: String,
     source :: dot,
     target :: dot
-  } deriving (Show, Eq)
+  }
+  deriving (Show, Eq)
 
 data Identity = Identity
   { lhs :: [String],
     rhs :: [String]
-  } deriving (Show, Eq)
+  }
+  deriving (Show, Eq)
 
 data Olog dot = Olog
   { dots :: [dot],
     arcs :: [Arc dot],
     identities :: [Identity]
-  } deriving (Show, Eq)
+  }
+  deriving (Show, Eq)
 
-makeOlog :: (Eq dot, Show dot) => [dot] -> [(String, dot, dot)] -> [Identity] -> Either String (Olog dot)
-makeOlog dots arcs identities = do
-  -- let sourceChecks = map (\arc -> arc.source `elem` dots && arc.target `elem` dots) arcs
-  -- let sourceChecks = map (\arc -> arc.source `elem` dots) arcs
-  -- let sourceErrors = catMaybes $ map (\source -> if source `elem` dots then Nothing else Just source) $ map (\arc -> arc.source ) arcs
-  -- let sourceErrors = mapMaybe (\source -> if source `elem` dots then Nothing else Just source) (map (\arc -> arc.source ) arcs)
-  let sources = map (\(_,src,_) -> src) arcs
-  let targets = map (\(_,_,tgt) -> tgt) arcs
-  let inDots source = if source `elem` dots then Nothing else Just source
-  let sourceErrors = mapMaybe inDots sources
-  let targetErrors = mapMaybe inDots targets
-  let mapArcs = map \(name, src, tgt) -> Arc { name=name, source=src, target=tgt }
-  case sourceErrors of
-      [] -> case targetErrors of
-        [] -> Right $ Olog dots (mapArcs arcs) identities
-        e : _ -> Left $ "bad target: " <> show e
-      e : _ -> Left $ "bad source: " <> show e
-  -- sourceFail match 
-  --   Just failingO => Left helpfulMessage
-  --   Nothing =>
-  --   targetFail match 
-  --   Just failingO => Left helpfulMessage
-  --     Nothing =>
-  --    Right olof
-  -- if sanity
-  --   then _
-  --   else Left _
+makeOlog :: forall dot. (Eq dot, Show dot) => [dot] -> [(String, dot, dot)] -> [Identity] -> Either String (Olog dot)
+makeOlog dots arcs identities =
+  case errors of
+    [] -> Right $ Olog dots (map (\(name, src, tgt) -> Arc {name = name, source = src, target = tgt}) arcs) identities
+    err : _ -> Left err
+  where
+    errors =
+      concat @[] @String $
+        concat @Maybe @[String] $
+        -- (concat :: (Maybe [[String]] -> [[String]])) $
+          traverse
+            ( sequence . \(dotMapper, errorPrefix) ->
+                map ((\dot -> if dot `elem` dots then Nothing else Just $ errorPrefix <> show dot) . dotMapper) arcs
+            )
+            [ (\(_, src, _) -> src, "bad source: "),
+              (\(_, _, tgt) -> tgt, "bad target: ")
+            ]
