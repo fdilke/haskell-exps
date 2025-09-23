f3456cd7dc5012793b8083767383ba2f5d26f490
fdilke <fdilke@gmail.com>
2025-09-23 16:40:51 +0100

    enforcing valid ologs
    Co-authored-by: George Thomas <georgefsthomas@gmail.com>

--- lib/LangFeature/Ologs.hs
+++ lib/LangFeature/Ologs.hs
@@ -1,37 +1,66 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedRecordDot #-}
-{-# HLINT ignore "Eta reduce" #-}
-{-# HLINT ignore "Avoid lambda" #-}
-{-# HLINT ignore "Redundant lambda" #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE NoFieldSelectors #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
+{-# LANGUAGE BlockArguments #-}
+{-# HLINT ignore "Eta reduce" #-}
+{-# HLINT ignore "Avoid lambda" #-}
+{-# HLINT ignore "Redundant lambda" #-}
 {-# HLINT ignore "Collapse lambdas" #-}
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
 
-module LangFeature.Ologs where
+module LangFeature.Ologs
+  ( Arc,
+    Identity,
+    Olog,
+    makeOlog,
+  )
+where
 
--- import GHC.StaticPtr (staticPtrKeys)
+import Data.Maybe
 
-doNothing :: Int -> Int
-doNothing x = x
+data Arc dot = Arc
+  { name :: String,
+    source :: dot,
+    target :: dot
+  } deriving (Show, Eq)
 
-data Arc dot = Arc {
-  arcId :: String,
-  source :: dot,
-  target :: dot
-  }
-data Identity = Identity {
-    lhs :: [String],
+data Identity = Identity
+  { lhs :: [String],
     rhs :: [String]
-}
+  } deriving (Show, Eq)
 
 data Olog dot = Olog
-  {
-    dots :: [dot],
+  { dots :: [dot],
     arcs :: [Arc dot],
     identities :: [Identity]
-  }
+  } deriving (Show, Eq)
 
-sanity :: forall dot. Eq dot => Olog dot -> Bool
-sanity olog = all (\arc -> arc.source `elem` olog.dots && arc.target `elem` olog.dots) olog.arcs
+makeOlog :: (Eq dot, Show dot) => [dot] -> [(String, dot, dot)] -> [Identity] -> Either String (Olog dot)
+makeOlog dots arcs identities = do
+  -- let sourceChecks = map (\arc -> arc.source `elem` dots && arc.target `elem` dots) arcs
+  -- let sourceChecks = map (\arc -> arc.source `elem` dots) arcs
+  -- let sourceErrors = catMaybes $ map (\source -> if source `elem` dots then Nothing else Just source) $ map (\arc -> arc.source ) arcs
+  -- let sourceErrors = mapMaybe (\source -> if source `elem` dots then Nothing else Just source) (map (\arc -> arc.source ) arcs)
+  let sources = map (\(_,src,_) -> src) arcs
+  let targets = map (\(_,_,tgt) -> tgt) arcs
+  let inDots source = if source `elem` dots then Nothing else Just source
+  let sourceErrors = mapMaybe inDots sources
+  let targetErrors = mapMaybe inDots targets
+  let mapArcs = map \(name, src, tgt) -> Arc { name=name, source=src, target=tgt }
+  case sourceErrors of
+      [] -> case targetErrors of
+        [] -> Right $ Olog dots (mapArcs arcs) identities
+        e : _ -> Left $ "bad target: " <> show e
+      e : _ -> Left $ "bad source: " <> show e
+  -- sourceFail match 
+  --   Just failingO => Left helpfulMessage
+  --   Nothing =>
+  --   targetFail match 
+  --   Just failingO => Left helpfulMessage
+  --     Nothing =>
+  --    Right olof
+  -- if sanity
+  --   then _
+  --   else Left _
--- test/Test/OlogsSpec.hs
+++ test/Test/OlogsSpec.hs
@@ -11,58 +11,41 @@ module Test.OlogsSpec where
 
 import LangFeature.Ologs
 import Test.Hspec
+import Data.Either (isRight)
+
+type MaybeOlog = Either String (Olog Int)
 
 spec :: Spec
 spec = do
-  describe "function successfully does very little" $ do
-    it "mimic (+1) function" $
-      doNothing 4 `shouldBe` 4
-
   describe "olog sanity checks" $ do
     it "arcs must have a source of a known dot" $
       let
-        badOlog :: Olog Int
+        badOlog :: MaybeOlog
         badOlog =
-          Olog
-            { dots = [0],
-              arcs = [Arc "source" 1 0],
-              identities = []
-            }
+          makeOlog [1] [("source", 0, 1)] [] 
       in
-        sanity badOlog `shouldBe` False
+        badOlog `shouldBe` Left "bad source: 0"
     it "arcs must have a target of a known dot" $
       let
-        badOlog :: Olog Int
+        badOlog :: MaybeOlog
         badOlog =
-          Olog
-            { dots = [0],
-              arcs = [Arc "source" 0 1],
-              identities = []
-            }
+          makeOlog [0] [("source", 0, 1)] []
       in
-        sanity badOlog `shouldBe` False
+        badOlog `shouldBe` Left "bad target: 1"
     it "arc is then ok" $
       let
-        goodOlog :: Olog Int
+        goodOlog :: MaybeOlog
         goodOlog =
-          Olog
-            { dots = [0, 1],
-              arcs = [Arc "source" 1 0, Arc "source" 0 1],
-              identities = []
-            }
+            makeOlog [0, 1] [("y", 1, 0), ("x", 0, 1)] []
       in
-        sanity goodOlog `shouldBe` True
+        isRight goodOlog `shouldBe` True
 
-  describe "create a basic olog" $ do
-    it "the graph olog" $
-      let -- x :: Int
-          -- x = 3
-          graphOlog :: Olog Int
-          graphOlog =
-            Olog
-              { dots = [0, 1],
-                arcs = [Arc "source" 0 1, Arc "target" 1 0],
-                identities = []
-              }
-      in do
-            [7, 8] `shouldBe` [7, 8]
+  -- describe "create a basic olog" $ do
+  --   it "the graph olog" $
+  --     let -- x :: Int
+  --         -- x = 3
+  --         graphOlog :: Either String (Olog Int)
+  --         graphOlog =
+  --           makeOlog [0, 1] [("source", 0, 1), ("target", 1, 0)] []
+  --     in do
+  --           [7, 8] `shouldBe` [7, 8]
